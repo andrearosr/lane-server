cacf22e0369441ce4dcd46527d60f7c2c2e330cc
Andrea Rosales <andrearosr@gmail.com>
2020-03-08 08:52:35 -0500

    Filter employees

--- src/graphql/resolvers/Company.js
+++ src/graphql/resolvers/Company.js
@@ -1,7 +1,20 @@
+import fs from 'fs';
+import util from 'util';
+import { getUser } from '../../helpers';
+
+const readDir = util.promisify(fs.readdir);
+
 export default {
   employees: async (root, args, { ctx }, info) => {
+    const files = await readDir('./data/users');
     let employees = [];
     // Fetch users with company ID === root.id
+
+    const userFiles = files.filter(filename => filename.includes('.json'))
+    
+    employees = await Promise
+      .all(userFiles.map(filename => getUser(filename.replace('.json', ''))))
+      .then(result => result.filter(user => user.company === root.id));
     
     return employees;
   }
